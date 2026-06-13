21a87f15551837c324c5f064e8c7eeca8c716729
MHarrisT <harristabassum151@gmail.com>
2026-06-13 23:25:35 +0500

    Added notes for Lecture 5

---
+++ Lec5_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // use syntax based on modern js
+
+console.log(3+3) // this will show output of 3+3
+// alert(3+3) this will not show output as we are coding in compiler not on console
+
+let name = "ALI"
+let age = 19
+let isHuman = true
+
+// number range is 2^53, if larger number than this then use bigint
+// string => ""
+// boolean = true/false
+// null = standalone value
+// undefined => has no value, not defined any value, you have just defined a variable
+// unique => symbol
+
+let state = null
+let new_state
+
+console.log(typeof undefined) // shows undefined
+console.log(typeof null) // shows object
+console.log(typeof isHuman) // shows boolean
