fcab2266d409a34b1dafd18a077fa532a455a54c
MHarrisT <harristabassum151@gmail.com>
2026-06-13 23:24:38 +0500

    Added notes for Lecture 4

---
+++ Lec4_variables.js
@@ -0,0 +1,12 @@
+const studentID = 1234
+let studentName = "Abid" 
+var cgpa = 3.2
+city = "Lahore"
+
+// studentID = 5678 (cannot change constant)
+
+// prefer not to use var because of block and function scope
+
+console.log(studentID)
+
+console.table([studentID, studentName, cgpa, city])
