8c99c4bec2ce9add7239a14dfc2aea85822a1f96
MHarrisT <harristabassum151@gmail.com>
2026-06-25 19:39:40 +0500

    Added notes for Lecture 28

---
+++ Lec28_while-do-while-loop.js
@@ -0,0 +1,21 @@
+// while loop
+
+let i=0
+// while: first condition checked and then loop executes
+while (i < 10){
+    console.log(i)
+    i++
+}
+
+let arr = [1,2,3,4,5,6,7]
+let index = 0
+while (index < arr.length){
+    console.log(arr[index++])
+}
+
+// do while: first loop executes then condition is checked
+let j = 0;
+do {
+    console.log(j)
+    j++
+} while (j < 10)
